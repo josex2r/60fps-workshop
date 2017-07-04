ad6f7f09646d4c3414e476699be5a605897f0c29
Jose Luis Represa <joseluis.represa@bbva.com>
2017-07-04 09:58:39 +0200

    publish

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,10 +1,13 @@
+// Array reverse for jQuery selectors.
+jQuery.fn.reverse = [].reverse;
+
 $(document).ready(() => {
   const $a = $('#navbar li a');
   let $el = $($a[0]).parent();
   const path = window.location.pathname.replace(/(^\/)|(\/$)|(\/raf)/g, '') || '/';
   const regExp = new RegExp(path, 'i');
 
-  $a.each((index, el) => {
+  $a.reverse().each((index, el) => {
     const href = el.href;
 
     if (href.match(regExp)) {
