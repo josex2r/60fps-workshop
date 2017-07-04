3d546ea777cdb22a4a481ea6646911dbae68e849
Jose Luis Represa <joseluis.represa@bbva.com>
2017-07-04 09:20:24 +0200

    vanilla appendChild

--- public/dom-render/raf.js
+++ public/dom-render/raf.js
@@ -1,12 +1,12 @@
 let $btnRunTask;
 let $progress;
-let $table;
+let table;
 let progressTimer;
 
 $(document).ready(function() {
   $btnRunTask = $('#btnRunTask');
   $progress = $('#progress');
-  $table = $('#table');
+  table = document.getElementById('table');
 
   bindRunTask();
   startProgressAnimation(5000, startProgressAnimation);
@@ -19,7 +19,7 @@ function bindRunTask() {
 }
 
 function startProgressAnimation(delay, callback) {
-  $progress.width('0%');
+  $progress[0].style.width ='0%';
 
   $progress.animate({
       width: '100%'
@@ -49,11 +49,16 @@ function renderRow(index, rows) {
   const row = rows[index];
 
   window.requestAnimationFrame(() => {
-    const content = row.reduce((acc, column) => {
-      return acc +`<td>${column}</td>`;
-    }, '');
+    const tr = document.createElement('tr');
+    
+    row.forEach((column) => {
+      const td = document.createElement('td');
 
-    $table.append(`<tr>${content}</tr>`);
+      td.innerHTML = column;
+      tr.appendChild(td);
+    });
+
+    table.appendChild(tr);
 
     if (index++ < rows.length -1) {
       renderRow(index, rows);
